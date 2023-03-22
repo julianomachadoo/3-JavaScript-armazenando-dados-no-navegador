c7eae5f40cac3b6b0e9b0c7dfa39236269455a67
julianomachadoo <julmachadoramos@gmail.com>
2023-03-22 12:18:52 -0300

    consultando dados no localStorage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+
+itens.forEach( (elemento) => {
+    console.log(elemento.nome, elemento.quantidade);
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
@@ -32,5 +37,6 @@ function criaElemento(nome, quantidade) {
 
     itens.push(itemAtual)
 
-    localStorage.setItem("item", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
+
 }
